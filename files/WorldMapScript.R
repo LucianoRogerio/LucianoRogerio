780005ceeb1154e128c6b3c11f2f389c79fad808
LucianoRogerio <lucianorogerio@gmail.com>
2024-03-25 22:36:11 -0300

    New Publications 2023

--- files/WorldMapScript.R
+++ files/WorldMapScript.R
@@ -13,18 +13,21 @@ m <- leaflet() %>%
 m
 ### use the export tool of Rstudio on the viewer tab at the right --->
 
-
 library(tidyverse)
 library(leaflet)
 
-
-
-LocSyn <- read.table(here::here("files", "Locations.csv"), header = T, sep = ",")
-
-
+LocSyn <- read.table(here::here("files", "LocationsSC.csv"), header = T, sep = ",")
 
 m <-  leaflet() %>%
   addTiles() %>%
   addCircleMarkers(lng = LocSyn$Long, lat = LocSyn$Lat, color = LocSyn$Color,
                    label = LocSyn$Location)
+m
+
+LocLet <- read.table(here::here("files", "MapaLetticia.csv"), sep = ",", header = T)
+
+head(LocLet)
+m <-  leaflet() %>%
+  addTiles() %>%
+  addCircles(lng = LocLet$Long, lat = LocLet$Lat, color = LocLet$Color)
 m
